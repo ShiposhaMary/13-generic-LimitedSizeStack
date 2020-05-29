9a93cbb30ca4998cf81c60c5ba1e81de21966944
Mary Shiposha <rusya7891@mail.ru>
2020-05-30 01:55:10 +0300

    Добавьте файлы проекта.

---
+++ My_LimitedSizeStack.cs
@@ -0,0 +1,43 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApplication
+{ /*стек ограниченного размера. Этот стек работает как обычный стек, 
+    однако при превышении максимального размера удаляет самый глубокий
+    элемент в стеке. Таким образом в стеке всегда будет
+    ограниченное число элементов.*/
+    public class LimitedSizeStack<T>
+    {
+        LinkedList<T> list = new LinkedList<T>();
+        private int limit;
+        public int Count { get; private set; }
+        public LimitedSizeStack(int limit)
+        {
+            this.limit = limit;
+            Count = 0;
+        }
+
+        public void Push(T item)
+        {
+            if (list.Count==limit)
+            { list.RemoveFirst();
+                Count--;
+            }
+            list.AddLast(item);
+            Count++;
+        }
+
+        public T Pop()
+        {
+           if(list.Count==0) throw new NotImplementedException();
+            T result = list.Last.Value;
+            list.RemoveLast();
+            Count--;
+            return result;
+        }
+        
+    }
+}
---
+++ My_ListModel.cs
@@ -0,0 +1,61 @@
+﻿ using System;
+using System.Collections.Generic;
+
+namespace TodoApplication
+{   
+
+    public class ListModel<TItem>
+    {
+        public List<TItem> Items { get; }
+        public int Limit;
+        LimitedSizeStack<(string motion,TItem item,int index) > commandsHistory;
+        
+
+
+        public ListModel(int limit)
+        {
+            Items = new List<TItem>();
+            Limit = limit;
+            commandsHistory = new LimitedSizeStack<(string motion, TItem item, int index)>(limit);
+           
+
+        }
+
+        public void AddItem(TItem item)
+        {
+
+            Items.Add(item);
+            commandsHistory.Push(("add",item,(Items.Count-1)));
+        }
+
+        public void RemoveItem(int index)
+        {
+            commandsHistory.Push(("remove", Items[index], index));
+            Items.RemoveAt(index);
+            
+
+        }
+
+        public bool CanUndo()
+        {
+            if (commandsHistory.Count > 0)
+                return true;
+            else
+                return false; 
+        }
+
+        public void Undo()
+        { if (CanUndo())
+            {
+              var undoCommand = commandsHistory.Pop();
+                if (undoCommand.motion == "remove") Items.Insert(undoCommand.index, undoCommand.item);
+                else Items.RemoveAt(undoCommand.index);
+            }
+            else      throw new NotImplementedException();
+
+            
+        }
+
+       
+    }
+}
